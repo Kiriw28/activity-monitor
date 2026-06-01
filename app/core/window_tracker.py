56f9d505a7cee2df2db51f2a540dd34e26e44599
Kiriw28 <googkirik@gmail.com>
2026-06-01 21:58:28 +0300

    Добавлена функция получения данных об активном окне

--- app/core/window_tracker.py
+++ app/core/window_tracker.py
@@ -1,19 +1,31 @@
-import logging
+import psutil
+import win32gui, win32process
 
-logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
-logging.debug("A DEBUG Message")
-logging.info("An INFO")
-logging.warning("A WARNING")
-logging.error("An ERROR")
-logging.critical("A message of CRITICAL severity")
 
+
+# Получение данных об активном окне
 def get_active_window_info():
-    active_window = "11111111111111111111111111111111"
+
+    hwnd = win32gui.GetForegroundWindow() # Получение HWND
+    title_window = win32gui.GetWindowText(hwnd) # Данные окна
+    getting_pid_win_process = win32process.GetWindowThreadProcessId(hwnd) # Получение идентификатора потока и процесса
+    pid_win_process = getting_pid_win_process[1]
+    piece_title_window = title_window.split(" - ")
+    app_name = piece_title_window[-1]
+
+    return pid_win_process, app_name
 
 
-    return print(active_window)
+# Получение списка активных процессов
+def get_all_pids_and_names_peocesses():
+    activities = []
+    for proc in psutil.process_iter(['pid', 'name']):
+        activities.append({proc.pid: proc.name()})
+    
+    return activities
 
 
-get_active_window_info()
+
+
+
 
