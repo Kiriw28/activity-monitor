f9464bee004251675a99363d0a52af9ea43aba8a
Kiriw28 <googkirik@gmail.com>
2026-06-01 22:40:02 +0300

    Правка логики функции get_active_window_info

--- app/core/window_tracker.py
+++ app/core/window_tracker.py
@@ -8,24 +8,13 @@ def get_active_window_info():
 
     hwnd = win32gui.GetForegroundWindow() # Получение HWND
     title_window = win32gui.GetWindowText(hwnd) # Данные окна
-    getting_pid_win_process = win32process.GetWindowThreadProcessId(hwnd) # Получение идентификатора потока и процесса
-    pid_win_process = getting_pid_win_process[1]
-    piece_title_window = title_window.split(" - ")
-    app_name = piece_title_window[-1]
-
-    return pid_win_process, app_name
+    _, pid = win32process.GetWindowThreadProcessId(hwnd) # Получение идентификатора потока и процесса
+    process = psutil.Process(pid)
+    process_name = process.name()
 
 
-# Получение списка активных процессов
-def get_all_pids_and_names_peocesses():
-    activities = []
-    for proc in psutil.process_iter(['pid', 'name']):
-        activities.append({proc.pid: proc.name()})
-    
-    return activities
-
-
-
-
-
-
+    return {
+        "pid": pid,
+        "process_name": process_name,
+        "window_title": title_window
+    }
---
+++ app/main.py
@@ -0,0 +1,5 @@
+import time
+from core.window_tracker import get_active_window_info
+
+
+
